36cc5c228d410eb2ca9d43ebc7438a4eadeeca22
TiffanyMaina <tiff_shiro@DESKTOP-Q44J8P0.>
2024-01-19 21:46:18 +0300

    completed

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -44,20 +44,37 @@ def print_spicy_foods(spicy_foods):
         food = x.get("name")
         emoji = x.get("heat_level") * "🌶"
         cuisine = x.get("cuisine")
-        x.update({"heat_level" : f"{emoji}"})
-        new_spicy.append({f"{food} ({cuisine}) | heat_level: {emoji}"})
-    list = f"{new_spicy[0]}         \n" + f"{new_spicy[1]}   \n" + f"{new_spicy[2]}      \n"
-    return list
+        # x.update({"heat_level" : f"{emoji}"})
+        new_spicy.append(f"{food} ({cuisine}) | Heat Level: {emoji}")
+        foodie = f"{new_spicy[-1]}"
+        print(foodie)
 
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
     pass
+    for x in spicy_foods:
+        deli_cusy = x.get("cuisine")
+        if deli_cusy == cuisine:
+            return x
 
 def print_spiciest_foods(spicy_foods):
     pass
+    #  print_spicy_foods(emoji) + get_spiciest_foods(>5)
+    # get spiciest foods and print with emojis
+    spiciest = get_spiciest_foods(spicy_foods)
+    return print_spicy_foods(spiciest)
+
 
 def get_average_heat_level(spicy_foods):
     pass
+    ave_lvl = []
+    for x in spicy_foods:
+        val = x.get("heat_level")
+        ave_lvl.append(val)
+    done = (ave_lvl[0] + ave_lvl[1] + ave_lvl[2])/len(ave_lvl)
+    return done
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
